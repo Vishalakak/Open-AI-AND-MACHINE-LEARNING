1af5be54ee96113520fe4f41a1e0e10720b792ef
Vishalakak <154723784+Vishalakak@users.noreply.github.com>
2025-03-06 15:55:24 +0530

    Delete ex3.py

--- ex3.py
+++
@@ -1,8 +0,0 @@
-import cv2
-img = cv2.imread('gan.jpg')
-grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imwrite('gan.jpg', grayImg)
-cv2.imshow('gan Gray', grayImg)
-cv2.imshow('gan', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
